9fb9f83eeea2ed7988f59b1a7824761c6107758f
krishna <krisnas9792@gmail.com>
2023-05-25 09:54:42 +0530

    adding js static file and making some changes to template file

--- Static/js/scripts.js
+++ Static/js/scripts.js
@@ -27,3 +27,10 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+function doThing(event) {
+  event.preventDefault();
+  window.confirm('Do you want to delete this post?') ? 
+    window.location.href="/delete/{{post.sno}}" :
+    null;
+};
